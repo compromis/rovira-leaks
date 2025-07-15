8a0e34692ac8431ecbb26ce7ab49386b59174aca
Guillem <guillem@disedit.com>
2025-07-15 14:49:47 +0200

    update numbers

--- pages/cas.vue
+++ pages/cas.vue
@@ -29,9 +29,9 @@ useSeoMeta({
         <Languages v-motion-slide-visible-bottom :delay="100" />
 
         <div v-motion-slide-visible-bottom :delay="200">
-          <p>El pitjor Conseller d’Educació de la història, José Antonio Rovira, ja s’ha gastat més de <Annotated type="highlight" inmediate>11.968€ dels nostres impostos en dinars i sopars</Annotated>. Convidant a persones que no sabem qui son en bars, restaurants, i marisqueries. Mentrestant, alguns professors han de pagar el material escolar de la seua butxaca.</p>
+          <p>El pitjor Conseller d’Educació de la història, José Antonio Rovira, ja s’ha gastat més de <Annotated type="highlight" inmediate>12.000€ dels nostres impostos en dinars i sopars</Annotated>. Convidant a persones que no sabem qui son en bars, restaurants, i marisqueries. Mentrestant, alguns professors han de pagar el material escolar de la seua butxaca.</p>
           <p>Sense publicar les factures al portal de transparència. Sense que consten reunions en l’agenda oficial. <Annotated>I tot això mentres cobra un sou de 85.000€ anuals</Annotated>.</p>
-          <p><Annotated type="highlight">PP i Vox volen canviar la llei per a amagar esta informació</Annotated>. Per això des de Compromís fem públics els <Annotated>84 tickets</Annotated> que demostren com els amics de Mazón utilitzen la Generalitat com el seu caixer automàtic en lloc de millorar la vida dels valencians i valencianes.</p>
+          <p><Annotated type="highlight">PP i Vox volen canviar la llei per a amagar esta informació</Annotated>. Per això des de Compromís fem públics els <Annotated>90 tickets</Annotated> que demostren com els amics de Mazón utilitzen la Generalitat com el seu caixer automàtic en lloc de millorar la vida dels valencians i valencianes.</p>
         </div>
       </section>
     </div>
